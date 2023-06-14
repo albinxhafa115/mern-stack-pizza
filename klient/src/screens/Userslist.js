7cb21082d5251e7015b01ce016010cb8abfa7809
albinxhafa115 <ax51756@ubt-uni.net>
2023-06-14 19:16:23 +0200

    Created UsersList.js

---
+++ klient/src/screens/Userslist.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import { deleteUser, getAllUsers } from '../actions/userActions';
+export default function Userslist() {
+    const userstate = useSelector((state) => state.loginUserReducer);
+    const { currentUser } = userstate;
+    const dispatch = useDispatch()
+    const getuserstate = useSelector((state) => state.getAllUsersReducer)
+    const { loading, error, users } = getuserstate
+
+    useEffect(() => {
+        dispatch(getAllUsers())
+        if (!currentUser.isAdmin) {
+            window.location.href = '/'
+        }
+    }, [])
+    return (
+        <div>
+            <div>
+                <div className='row justify-content-center'>
+                    <div className='col-md-10'>
+                        <h2 style={{ fontSize: '25px' }}>Admin Panel</h2>
+                        <ul className='adminfunctions'>
+                            <li><Link to={'/admin/userslist'}>Users List</Link></li>
+                            <li><Link to={'/admin/pizzaslist'}>Pizzas List</Link></li>
+                            <li><Link to={'/admin/addpizza'}>Add New Pizza</Link></li>
+                            <li><Link to={'/admin/orderslist'}>Orders List</Link></li>
+                        </ul>
+
+
+
+                    </div>
+                </div>
+            </div>
+
+            <h1>Users List</h1>
+            {loading && (<Loading />)}
+            {error && (<Error error='Something went wrong' />)}
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>User Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users && users.map(user => {
+                        return <tr>
+                            <td>{user._id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td><i className='fa fa-trash' onClick={() => (dispatch(deleteUser(user._id)))}></i></td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
