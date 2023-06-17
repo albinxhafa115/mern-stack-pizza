35d670ad8b1d9a231b56f152a03117b2d6760363
albinxhafa115 <ax51756@ubt-uni.net>
2023-06-17 19:51:56 +0200

    Updated userRoute.js ,added insert into mysql when user click register  button

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,76 +1,75 @@
-const express = require("express");
-const userModel = require("../models/userModel");
+const express = require('express');
 const router = express.Router();
-const User = require("../models/userModel")
+const User = require('../models/userModel');
+const mysqlConnection = require('../dbconnect.js');
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
 
-    const { name, email, password } = req.body
-
-    const newUser = new User({ name, email, password })
+    // Insert new user into MongoDB
+    const newUser = new User({ name, email, password });
 
     try {
-        newUser.save()
-        res.send('User Registered with success')
+        await newUser.save();
+        console.log('User Registered with success in MongoDB');
     } catch (error) {
+        console.error('Error creating new user in MongoDB: ', error);
         return res.status(400).json({ message: error });
     }
 
+    // Insert new user into MySQL
+    const query = `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`;
+    const values = [name, email, password];
 
-})
+    mysqlConnection.query(query, values, (error, results, fields) => {
+        if (error) {
+            console.error('Error creating new user in MySQL: ', error);
+            return res.status(400).json({ message: error });
+        }
+        console.log('New user created with ID: ', results.insertId);
+        res.send('User Registered with success in MySQL');
+    });
+});
 
-
-router.post("/login", async (req, res) => {
-    const { email, password } = req.body
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
     try {
-        const user = await User.find({ email, password })
+        const user = await User.find({ email, password });
 
         if (user.length > 0) {
             const currentUser = {
                 name: user[0].name,
                 email: user[0].email,
                 isAdmin: user[0].isAdmin,
-                _id: user[0]._id
-            }
+                _id: user[0]._id,
+            };
             res.send(currentUser);
-        }
-        else {
+        } else {
             return res.status(400).json({ message: 'User Login Failed' });
         }
     } catch (error) {
         return res.status(400).json({ message: 'Something went wrong' });
-
     }
-})
+});
 
-router.get("/getallusers", async (req, res) => {
+router.get('/getallusers', async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
+        const users = await User.find({});
+        res.send(users);
     } catch (error) {
         return res.status(400).json({ message: error });
-
-
     }
+});
 
-
-})
-
-router.post("/deleteuser", async (req, res) => {
-
-    const userid = req.body.userid
+router.post('/deleteuser', async (req, res) => {
+    const userid = req.body.userid;
 
     try {
-        await User.findOneAndDelete({ _id: userid })
-        res.send('User deleted Successfully')
+        await User.findOneAndDelete({ _id: userid });
+        res.send('User deleted Successfully');
     } catch (error) {
         return res.status(400).json({ message: error });
-
     }
+});
 
-})
-
-
-
-
-module.exports = router
+module.exports = router;
